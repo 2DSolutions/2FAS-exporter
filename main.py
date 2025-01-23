3837e052a10b1c4f74e13f98500e2e78efbcc036
Zinoto <david@deschanel.fr>
2025-01-23 18:43:29 +0100

    Add arg parser and clean main.py

--- main.py
+++ main.py
@@ -1,42 +1,18 @@
-import json
-import qrcode
-import os
+import sys, os
+from twofas_lib import generate_qr_codes
 
-from twofas_lib import QRCode
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <source_json_file> <destination_directory>")
+        sys.exit(1)
 
-def generate_qr_codes(file_path):
-    # Create output directory if it doesn't exist
-    output_dir = "qr_codes"
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    source_file = sys.argv[1]
+    destination_dir = sys.argv[2]
 
-    # Load json file
-    with open(file_path, "r") as file:
-        data = json.load(file)
+    if not os.path.exists(source_file):
+        print(f"Source file {source_file} does not exist.")
+        sys.exit(1)
 
-    # Parse json file and generate QR codes for each service
-    for service in data["services"]:
-        # Generate QrCode object
-        try:
-            qr_code = QRCode(
-                    secret=service["secret"],
-                    issuer=service["otp"]["issuer"],
-                    tokenType=service["otp"]["tokenType"],
-                    digits=service["otp"]["digits"] if "digits" in service["otp"] else "6",
-                    period=service["otp"]["period"] if "period" in service["otp"] else "",
-                    algorithm=service["otp"]["algorithm"] if "algorithm" in service["otp"] else "SHA1",
-                    account=service["otp"]["account"],
-                    )
-            
-            # Generate QR code based on QrCode OTPAuth URL
-            qr_img = qrcode.make(qr_code.otpauth)
-            output_file = os.path.join(output_dir, f"{qr_code.issuer}.png")
-            qr_img.save(output_file)
-
-            print(f"QRCode {qr_code.label} saved as {output_file}")
-            
-        except KeyError:
-            print(f"JSON file for {service['otp']['label']} is not properly formatted or a value is missing")
-                 
-#Generate QR codes for 2fastest.json
-generate_qr_codes(".env/2fastest.json")
+    if not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
+    generate_qr_codes(source_file, destination_dir)
--- twofas_lib.py
+++ twofas_lib.py
@@ -1,3 +1,7 @@
+import json
+import qrcode
+import os
+
 class QRCode:
     def __init__(self, issuer="", secret="", account="", digits="6", period="30", algorithm="SHA1", tokenType="TOTP"):
         self.issuer = issuer                # Account issuer
@@ -12,3 +16,37 @@ class QRCode:
         self.label = f"{issuer}:{account}"  # Account name / Account issuer @ Account email or username
         self.otpauth = f"otpauth://{self.tokenType}/{self.label}?secret={self.secret}&issuer={self.issuer}&digits={self.digits}&period={self.period}&algorithm={self.algorithm}"
     
+def generate_qr_codes(file_path,output_dir):
+    # Create output directory if it doesn't exist
+    
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Load json file
+    with open(file_path, "r") as file:
+        data = json.load(file)
+
+    # Parse json file and generate QR codes for each service
+    for service in data["services"]:
+        # Generate QrCode object
+        try:
+            qr_code = QRCode(
+                    secret=service["secret"],
+                    issuer=service["otp"]["issuer"],
+                    tokenType=service["otp"]["tokenType"],
+                    digits=service["otp"]["digits"] if "digits" in service["otp"] else "6",
+                    period=service["otp"]["period"] if "period" in service["otp"] else "",
+                    algorithm=service["otp"]["algorithm"] if "algorithm" in service["otp"] else "SHA1",
+                    account=service["otp"]["account"],
+                    )
+            
+            # Generate QR code based on QrCode OTPAuth URL
+            qr_img = qrcode.make(qr_code.otpauth)
+            output_file = os.path.join(output_dir, f"{qr_code.issuer}.png")
+            qr_img.save(output_file)
+
+            print(f"QRCode {qr_code.label} saved as {output_file}")
+            
+        except KeyError:
+            print(f"JSON file for {service['otp']['label']} is not properly formatted or a value is missing")
+                 
