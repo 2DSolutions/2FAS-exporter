8cb8c4c27d292ec872fe33c9a26a2e8316c02d15
Zinoto <david@deschanel.fr>
2025-01-24 11:30:21 +0100

    Change on dict key handling

--- twofas_lib.py
+++ twofas_lib.py
@@ -3,7 +3,7 @@ import qrcode
 import os
 
 class QRCode:
-    def __init__(self, issuer="", secret="", account="", digits="6", period="30", algorithm="SHA1", tokenType="TOTP"):
+    def __init__(self, issuer, secret, account, digits, period, algorithm, tokenType):
         self.issuer = issuer                # Account issuer
         self.tokenType = tokenType.lower()  # totp or hotp
         self.secret = secret                # Base32 encoded secret
@@ -13,7 +13,7 @@ class QRCode:
         self.period = period                # Period in seconds (default 30)
         self.algorithm = algorithm          # Hash algorithm (SHA1, SHA256, SHA512) (default SHA1)
        
-        self.label = f"{issuer}:{account}"  # Account name / Account issuer @ Account email or username
+        self.label = f"{issuer}:{account}"  # Account issuer:Account email or username
         self.otpauth = f"otpauth://{self.tokenType}/{self.label}?secret={self.secret}&issuer={self.issuer}&digits={self.digits}&period={self.period}&algorithm={self.algorithm}"
     
 def generate_qr_codes(file_path,output_dir):
@@ -29,14 +29,14 @@ def generate_qr_codes(file_path,output_dir):
         # Generate QrCode object
         try:
             qr_code = QRCode(
-                    secret=service["secret"],
-                    issuer=service["otp"]["issuer"] if "issuer" in service["otp"] else service["name"],
-                    tokenType=service["otp"]["tokenType"],
-                    digits=service["otp"]["digits"] if "digits" in service["otp"] else "6",
-                    period=service["otp"]["period"] if "period" in service["otp"] else "",
-                    algorithm=service["otp"]["algorithm"] if "algorithm" in service["otp"] else "SHA1",
-                    account=service["otp"]["account"] if "account" in service["otp"] else "",
-                    )
+                secret=service["secret"],
+                issuer=service["otp"].get("issuer", service["name"]),
+                tokenType=service["otp"]["tokenType"],
+                digits=service["otp"].get("digits", "6"),
+                period=service["otp"].get("period", "30"),
+                algorithm=service["otp"].get("algorithm", "SHA1"),
+                account=service["otp"].get("account", "")
+            )
             
             # Generate QR code based on QrCode OTPAuth URL
             qr_img = qrcode.make(qr_code.otpauth)
